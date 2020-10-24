f5951ad159ed6691a9101262cbf90fb05afc103e
rafael <rapha.pse@outlook.com>
2020-10-23 21:00:23 -0300

    Send Build for Heroku

---
+++ build/precache-manifest.c69b59f640c4ae31be9ca00a7a5e43ed.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50578e47bc2ee16d6b84676e3a5bab75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc55191256df833faa68",
+    "url": "/static/css/main.e16a1e4c.chunk.css"
+  },
+  {
+    "revision": "165cf74cac7761202fff",
+    "url": "/static/js/2.5393cac6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5393cac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc55191256df833faa68",
+    "url": "/static/js/main.8f0e0df3.chunk.js"
+  },
+  {
+    "revision": "9bba1f7a403374db5104",
+    "url": "/static/js/runtime-main.a6d678b9.js"
+  },
+  {
+    "revision": "7d346ad99170cabe9aeaa5574d6bb864",
+    "url": "/static/media/back.7d346ad9.svg"
+  },
+  {
+    "revision": "128d3c9b10f7833af300ca0e6e444fb4",
+    "url": "/static/media/close_model.128d3c9b.svg"
+  },
+  {
+    "revision": "c6da832ae61a69c74ebe7ee62245384c",
+    "url": "/static/media/cloud.c6da832a.svg"
+  },
+  {
+    "revision": "54b4be313496484de14dcf73613e1a34",
+    "url": "/static/media/colors.54b4be31.svg"
+  },
+  {
+    "revision": "4d902463e7d30d1c91acc269ecc1cc2e",
+    "url": "/static/media/experiments.4d902463.svg"
+  },
+  {
+    "revision": "d4e5035120de293b60f516655d2e20b2",
+    "url": "/static/media/file.d4e50351.svg"
+  },
+  {
+    "revision": "62e982c094b399609a2b4e9b14a90ca3",
+    "url": "/static/media/logo.62e982c0.svg"
+  },
+  {
+    "revision": "00e4502691957770cead37685fee3b46",
+    "url": "/static/media/logout.00e45026.svg"
+  },
+  {
+    "revision": "502f35796a9261935d7644480f9dacdc",
+    "url": "/static/media/next.502f3579.svg"
+  },
+  {
+    "revision": "c22a7b412ca6f7390f6410811573350a",
+    "url": "/static/media/options_dot.c22a7b41.svg"
+  },
+  {
+    "revision": "0c0470265834f165bae5d6193378ab27",
+    "url": "/static/media/ownership.0c047026.svg"
+  },
+  {
+    "revision": "56402b1d6cf2027fb80a330b05784ad0",
+    "url": "/static/media/security.56402b1d.svg"
+  },
+  {
+    "revision": "5fb7a756f57a6cea4ab15deb1243194d",
+    "url": "/static/media/sketch.5fb7a756.svg"
+  },
+  {
+    "revision": "5ce76ef5ef50f93752f7e6903065ee5e",
+    "url": "/static/media/test.5ce76ef5.svg"
+  }
+]);
